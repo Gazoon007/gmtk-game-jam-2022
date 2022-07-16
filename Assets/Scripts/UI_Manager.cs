73b331929ba5a700b5188f0f4ddd39ba0ba554aa
CC56785 <christianilzhoefer@gmail.com>
2022-07-16 19:16:46 +0200

    More UI Elements

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class UI_Manager : MonoBehaviour
 {
 	private Transform activeMenu;
+	private Transform menuBackgroundImage;
 	private Transform mainMenu;
 	private Transform creditsMenu;
 	private Transform levelSelectMenu;
 	private Transform gameOverlay;
 
+	private int numberOfLeftWeaponSwaps;
+
 	void Start()
 	{
 		Transform UI = GameObject.Find("UI").transform.GetChild(0);
+		menuBackgroundImage = UI.GetChild(0);
 		mainMenu = UI.GetChild(1);
 		creditsMenu = UI.GetChild(2);
 		levelSelectMenu = UI.GetChild(3);
 		gameOverlay = UI.GetChild(4);
 
 		activeMenu = mainMenu;
+
+		numberOfLeftWeaponSwaps = 2;
+		SwapWeapon(false);
+		ChangeEnemyCount(3);
 	}
 
 	public void Credits()
@@ -48,10 +57,40 @@ public class UI_Manager : MonoBehaviour
 	public void StartLevel(int level)
 	{
 		activeMenu.gameObject.SetActive(false);
+		menuBackgroundImage.gameObject.SetActive(false);
 
 		activeMenu = gameOverlay;
 		gameOverlay.gameObject.SetActive(true);
 
 		Debug.Log("Level " + level + " started");
+	}
+
+	// Call this to update the number in the top left corner
+	public void SwapWeapon(bool subtractOne)
+	{
+		if (subtractOne)
+		{
+			numberOfLeftWeaponSwaps--;
+		}
+		gameOverlay.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = numberOfLeftWeaponSwaps.ToString();
+	}
+
+	// Call this to change the text at the top
+	public void ChangeMoveState(bool itIsNowThePlayersTurn)
+	{
+		if (itIsNowThePlayersTurn)
+		{
+			gameOverlay.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Your Move";
+		}
+		else
+		{
+			gameOverlay.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Enemy's Move";
+		}
+	}
+
+	//Call this to change the number in the top right corner
+	public void ChangeEnemyCount(int numberOfEnemys)
+	{
+		gameOverlay.GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>().text = numberOfEnemys.ToString();
 	}
 }
